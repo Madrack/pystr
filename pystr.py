59d9bb3497372d49a602b0eacd7c5df4424de335
Madrack <Madrack@IT-14.brd.local>
2023-06-23 16:19:00 +0300

    first commit

---
+++ pystr.py
@@ -0,0 +1,15 @@
+import re
+
+st = re.compile(r' -?\d+')
+f_in = open('IR.csv','r')
+f_out = open('IR_out.csv','w')
+myStr = ''
+for line in f_in: 
+    data_matches = st.findall(line)
+    # numbers = [str(num) for num in data_matches]
+    if len(data_matches) != 0: 
+        myStr += ', '.join(data_matches) 
+    else:
+        f_out.write(myStr+'\n') 
+        myStr = ''
+f_out.close()
